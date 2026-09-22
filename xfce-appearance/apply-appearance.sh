0cec36237428347a9d5efea4c9e7b2fb54bfaff9
jcolman <jc@irc.be>
2026-09-22 16:13:10 +0200

    Disable compositing/transparency for Citrix performance + fix p10k warning
    Picom + transparantie over Citrix veroorzaakte merkbare lag (veel netwerk-
    verkeer door constante scherm-updates). Picom autostart verwijderd, panels en
    kitty terug ondoorzichtig, xfwm4-compositing blijft uit. Thema/kleuren/wallpaper
    blijven behouden.
    
    Ook p10k INSTANT_PROMPT op 'quiet' gezet zodat de fastfetch-output bij start
    geen instant-prompt-warning meer geeft.

--- xfce-appearance/apply-appearance.sh
+++ xfce-appearance/apply-appearance.sh
@@ -6,10 +6,11 @@ xfconf-query -c xfwm4    -p /general/theme    -s "Tokyonight-Dark-Storm"
 # Icons:
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Colloid-dark"
 
-# --- Fase 3: panel transparency (Tokyo Night) ---
+# --- Compositing UIT (Citrix: geen picom/xfwm4-compositing i.v.m. lag) ---
+# Compositing + transparantie over Citrix veroorzaakt veel netwerkverkeer en
+# daardoor traagheid. Panels en terminal blijven daarom ondoorzichtig.
 xfconf-query -c xfwm4 -p /general/use_compositing -s false
 for p in panel-1 panel-2; do
-  xfconf-query -c xfce4-panel -p /panels/$p/background-style -s 1
-  xfconf-query -c xfce4-panel -p /panels/$p/background-rgba -s 0.102 -s 0.106 -s 0.149 -s 0.85
+  xfconf-query -c xfce4-panel -p /panels/$p/background-style -s 0
 done
 xfce4-panel -r
